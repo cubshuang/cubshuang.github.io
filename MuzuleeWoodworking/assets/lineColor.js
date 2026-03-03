c44043fbc9fe3cf85076293cb99685d6aa70fdd6
Huang Chun-Hung <30680380+cubshuang@users.noreply.github.com>
2026-03-04 00:15:38 +0800

    Add files via upload

---
+++ MuzuleeWoodworking/assets/lineColor.js
@@ -0,0 +1,25 @@
+//線條參數
+var boxColor=[
+    {color:"#DDC675",name:"牛皮"},    
+    {color:"#b3850d",name:"黃褐"},    
+    {color:"#5F3B07",name:"深棕"},
+    {color:"#7DFB60",name:"脆綠"},
+    {color:"#2CA707",name:"抹茶綠"},
+    {color:"#29441b",name:"橄欖綠"},
+    {color:"#ffdb14",name:"黃"},
+    {color:"#FAEEA0",name:"鵝黃"},
+    {color:"#FFC187",name:"粉橘"},
+    {color:"#E96300",name:"南瓜橘"},
+    // {color:"#E008DD",name:"桃紅"},
+    {color:"#C30000",name:"紅"},
+    {color:"#60c2ff",name:"粉藍"},
+    {color:"#119ADB",name:"水藍"},    
+    {color:"#074d81",name:"皇家藍"},    
+    {color:"#EEEEEE",name:"純白"},
+    {color:"#000000",name:"黑"},
+    {color:"#3b3b38",name:"深灰"}  
+   ];
+
+   //名稱顯示黑色字體
+   var boxlightColors=   ["#EEEEEE","#FAEEA0","#7DFB60","#ffdb14"]; //純白、鵝黃、脆綠、黃
+   
